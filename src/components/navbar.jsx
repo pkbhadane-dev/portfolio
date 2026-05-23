b1af9c42372c78a054ee81ab772cd48178bc3cc8
Prashant Bhadane <pkbhadane545@gmail.com>
2026-05-23 21:47:03 +0530

    Enhance Navbar with icon and styling improvements; update Contact and Projects sections for better layout and responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { IoReorderThree } from "react-icons/io5";
 import { Link } from "react-router";
 
 export const Navbar = () => {
@@ -12,12 +13,12 @@ export const Navbar = () => {
   ];
   return (
     <>
-      <div className="fixed z-120 top-0 left-0 right-0 mx-1.5 bg-transparent py-4">
-        <nav className="max-w-6xl mx-auto flex justify-between items-center">
-          <span className="text-primary font-bold hover:text-text-muted">
-            LOGO
+      <div className="fixed z-120 top-0 left-0 right-0  bg-transparent">
+        <nav className="max-w-6xl mx-auto glass rounded-b-md flex justify-between p-1 items-center">
+          <span className="text-primary pl-2 text-lg md:text-2xl font-bold">
+           {"<PB/>"}
           </span>
-          <div className=" glass hidden py-1 px-2 rounded-full sm:flex items-center gap-1">
+          <div className="hidden py-1 px-2 rounded-full sm:flex items-center gap-1">
             {navLinks.map((link, index) => (
               <span
                 className="px-4 py-2 rounded-full text-sm hover:text-text-muted hover:bg-bg"
@@ -28,7 +29,7 @@ export const Navbar = () => {
             ))}
           </div>
           <span onClick={() => setShowMenu(!showMenu)} className="sm:hidden">
-            ICON
+            <IoReorderThree size={32} className={`${showMenu && "scale-95 transition-all duration-300"}`} />
           </span>
         </nav>
 
--- src/sections/projects.jsx
+++ src/sections/projects.jsx
@@ -6,31 +6,63 @@ import { FaGithub } from "react-icons/fa";
 export const Projects = () => {
   return (
     <>
-      <div className="max-width m-auto px-4 flex flex-col gap-5 pt-16 overflow-hidden" id="projects">
+      <div
+        className="max-width m-auto md:px-4 flex flex-col gap-5 pt-16 overflow-hidden"
+        id="projects"
+      >
         <div>
-          <h3 className="text-primary">PROJECTS</h3>
+          <h3 className="text-primary px-2">PROJECTS</h3>
         </div>
         <div>
           <div className="grid md:grid-cols-2 gap-16 text-font p-4">
             {projects.map((project) => (
-              <div key={project.id} className="flex flex-col rounded-md overflow-hidden">
+              <div
+                key={project.id}
+                className="flex flex-col rounded-md overflow-hidden"
+              >
                 <div className="group glow m-1 rounded-md relative overflow-hidden h-full">
-                  <img className="w-full group-hover:scale-110 transition-transform duration-500" src={project.thumbnail} alt="" />
-                  <div className="absolute inset-0 group-hover:bg-linear-to-t from-surface via-surface/80 to-transparent opacity-50"/>
+                  <img
+                    className="w-full group-hover:scale-110 transition-transform duration-500"
+                    src={project.thumbnail}
+                    alt=""
+                  />
+                  <div className="absolute inset-0 group-hover:bg-linear-to-t from-surface via-surface/80 to-transparent opacity-50" />
                   <div className=" absolute inset-0 flex gap-5 justify-center items-center opacity-0 group-hover:opacity-100 transition-opacity duration-700">
-                    <a className=" group-hover:bg-surface/90 rounded-md" href={project.liveLink}>
-                      <ArrowUpRight size={28}/>
+                    <a
+                      className=" group-hover:bg-surface/90 rounded-md"
+                      href={project.liveLink}
+                    >
+                      <ArrowUpRight size={28} />
                     </a>
-                    <a className="group-hover:bg-surface/90 p-1 rounded-md" href={project.githubLink}>
-                      <FaGithub size={22}/>
+                    <a
+                      className="group-hover:bg-surface/90 p-1 rounded-md"
+                      href={project.githubLink}
+                    >
+                      <FaGithub size={22} />
                     </a>
                   </div>
                 </div>
+                <div className="flex xl:hidden justify-center gap-10 py-1 pb-2 font-serif">
+                  <button className=" flex glass pr-2 py-1 text-sm rounded-md justify-center items-center">
+                    <ArrowUpRight size={25} />
+                    <span>Live</span>
+                  </button>
+                  <button className="flex glass px-2 py-1 text-sm rounded-md gap-1 items-center justify-center">
+                    <FaGithub size={20} />
+                    <span>Github</span>
+                  </button>
+                </div>
                 <div className="glass p-2 px-3 space-y-2 border-none md:h-full ">
-                  <h4 className="text-primary font-semibold">{project.title}</h4>
+                  <h4 className="text-primary font-semibold">
+                    {project.title}
+                  </h4>
                   <p>{project.description}</p>
                   <div className="flex flex-wrap gap-3 pt-2">
-                    {project.techStack.map((tech)=><span className="glass py-1 px-2 rounded-md text-sm">{tech}</span>)}
+                    {project.techStack.map((tech) => (
+                      <span className="glass py-1 px-2 rounded-md text-sm">
+                        {tech}
+                      </span>
+                    ))}
                   </div>
                 </div>
               </div>
